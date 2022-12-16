b9b7000452b9f8a844b89e0a3339fb646336526d
weston-bailey <wdbailey87@gmail.com>
2022-12-16 11:26:06 -0800

    adds async/await example

--- index.js
+++ index.js
@@ -11,21 +11,34 @@ const API_KEY = process.env.API_KEY
 
 
 // routes
-app.get('/', (req, res) => {
-    const baseUrl = `https://www.omdbapi.com/?apikey=${API_KEY}&s=star+wars`
-    console.log(baseUrl)
-    axios.get(baseUrl)
-        .then(response => {
-            // you need to go into the response.data, that is where axios puts the API's response to us
-            // console.log(response.data)
-            res.json(response.data)
-        })
-        .catch(error => {
-            // console log the specifics of the error, but keep them private
-            console.log('🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥', error)
-            // generic interal server error code, message to the client that something bad happened
-            res.status(500).send('internal server error')
-        })
+app.get('/', async (req, res) => { // async the route to use the await keyword in it
+    // using axios with fetch
+    // const baseUrl = `https://www.omdbapi.com/?apikey=${API_KEY}&s=star+wars`
+    // console.log(baseUrl)
+    // axios.get(baseUrl)
+    //     .then(response => {
+    //         // you need to go into the response.data, that is where axios puts the API's response to us
+    //         // console.log(response.data)
+    //         res.json(response.data)
+    //     })
+    //     .catch(error => {
+    //         // console log the specifics of the error, but keep them private
+    //         console.log('🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥', error)
+    //         // generic interal server error code, message to the client that something bad happened
+    //         res.status(500).send('internal server error')
+    //     })
+    try {
+        const baseUrl = `https://www.omdbapi.com/?apikey=${API_KEY}&s=star+wars`
+        console.log(baseUrl)
+        const response = await axios.get(baseUrl)
+        res.json(response.data)
+    } catch (error) {
+        // console log the specifics of the error, but keep them private
+        console.log('🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥', error)
+        // generic interal server error code, message to the client that something bad happened
+        res.status(500).send('internal server error')
+        
+    }
 })
 
 // listening on a port
